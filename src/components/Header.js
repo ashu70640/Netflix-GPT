9147d29abee39370240edd8b9646cc41293be5c8
ashu70640 <ashu70640@gmail.com>
2024-07-16 23:51:05 +0530

    signIn/signUp form & Login/Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-20">
+      <img
+        className="w-40 m-4"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png?20190206123158"
+        alt="logo"
+      />
+    </div>
+  );
+};
